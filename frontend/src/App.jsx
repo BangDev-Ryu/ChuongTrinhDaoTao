3f4a95af94f145a552e0120b41fb6fbf951dd7b6
BangDev-Ryu <ryushiki12@gmail.com>
2025-05-15 23:56:33 +0700

    init project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="d-flex justify-content-center">
+        <Button variant="primary">haha</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
