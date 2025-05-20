73fa61246d8fb54e55aa4d73a2a08b406f8d6e41
Miryl <hntb0102@gmail.com>
2025-05-21 05:28:33 +0700

    backend HocPhan

---
+++ backend/src/main/java/com/chuongtrinhdaotao/backend/repositories/HocPhanRepository.java
@@ -0,0 +1,7 @@
+package com.chuongtrinhdaotao.backend.repositories;
+
+import com.chuongtrinhdaotao.backend.models.HocPhan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HocPhanRepository extends JpaRepository <HocPhan, Integer> {
+}
---
+++ backend/src/main/java/com/chuongtrinhdaotao/backend/services/HocPhanServiceImpl.java
@@ -0,0 +1,4 @@
+package com.chuongtrinhdaotao.backend.services;
+
+public class HocPhanServiceImpl {
+}
